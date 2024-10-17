445d53f15acf5ce32a17fc5035c66a16e03760ea
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-17 13:05:12 +0700

    update commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 3000;
 
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.render('index');
