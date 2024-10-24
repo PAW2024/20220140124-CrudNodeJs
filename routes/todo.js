656e009c8a9f7c796f5828e785e6a5084e748dff
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-24 12:21:52 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -7,6 +7,8 @@ let todos = [{
     {
         id: 2, task: "Membuat API", completed: false    
     },
+    {id: 3, task: "Ini adalah task  baru", completed: false
+    },
 ];
 
 router.get('/', (req, res) => {res.json(todos)});
@@ -18,6 +20,24 @@ router.post('/', (req, res) => {
     todos.push(newTodo);
     res.status(201).json(newTodo);
 
+});
+router.delete('/:id', (req,res) => {
+    const todoIndex = todos.findIndex(t=>t.id===parseInt(req.params.id));
+    if (todoIndex === -1) return res.status(404).json({message: 'Tugas tidak ditemukan'});
+
+    const deletedTodo = todos.splice(todoIndex, 1)[0];
+    res.status(200).json9({message: `Tugas'${deletedTodo.task}' telah dihapus`});
+});
+
+router.put('/:id', (req,res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json({ message: 'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbarui`,
+        updatedTodo: todo
+    });
 });
 
 module.exports = router;
